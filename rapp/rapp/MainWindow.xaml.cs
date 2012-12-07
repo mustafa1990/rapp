950b8bb94a9c2897c84338207399e4e2950bfccf
mustafa1990 <mustafa900910@gmail.com>
2012-12-07 13:48:19 +0100

    Revert "Fixed shit"
    This reverts commit e2320d9719fe72579a330a22478f2ffdce59c9a3.

--- rapp/rapp/MainWindow.xaml.cs
+++ rapp/rapp/MainWindow.xaml.cs
@@ -10,18 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace rapp
 {
     /// <summary>
-    /// Interaction logic for LoginWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
